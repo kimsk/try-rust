eec547ac471ee31c261cb23c17cce252d7a90940
Karlkim Suwamongkol <contact@karlk.im>
2021-01-03 23:17:26 +0700

    finish guessing_game

--- The-Rust-Programming-Language/guessing_game/src/main.rs
+++ The-Rust-Programming-Language/guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 // cargo doc --open
 // generates doc!
@@ -8,31 +8,34 @@ use rand::Rng;
 fn main() {
     println!("Guess the #");
 
-    let secret_number =
-        rand::thread_rng()
-            .gen_range(1..101);
-    
-    println!("The secret number is: {}", secret_number);
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    //println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess) // returns io::Result
-        .expect("Failed to read line"); // expect is required
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess) // returns io::Result
+            .expect("Failed to read line"); // expect is required
 
-    // convert a value of guess from string to u32
-    // using Shadowing
-    let guess: u32 =
-        guess
-            .trim()
-            .parse() // return Result
-            .expect("Please type a number!");
-    println!("You guessed: {}", guess);
+        // convert a value of guess from string to u32
+        // using Shadowing
+        let guess: u32 = 
+            match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue, // Use match expression to handling the error.
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+            };
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
