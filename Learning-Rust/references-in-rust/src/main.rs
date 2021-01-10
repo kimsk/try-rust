d7e6a9df7821f42b8bd2f6a72c2fc5d36b3566a4
Karlkim Suwamongkol <contact@karlk.im>
2021-01-10 16:46:21 +0700

    mutable borrows

--- Learning-Rust/references-in-rust/src/main.rs
+++ Learning-Rust/references-in-rust/src/main.rs
@@ -18,10 +18,6 @@ fn main() {
     // `.` operator performs the dereferencing implicitly
     println!("Hello, {}!", (*r).first_name);
 
-    let mut mutable_numbers: [i32;3] = [3, 1, 2];
-    mutable_numbers.sort(); // sort(&mut self) -- mutable borrow
-    println!("mutable_numbers {:?}", mutable_numbers);
-
     let r = &pascal; // borrow as immutable
     borrow_person(r);
     println!("pascal not changed {:#?}", pascal);
@@ -29,6 +25,29 @@ fn main() {
     println!("pascal.age changed from 28 -> {}", pascal.age);
     move_person(pascal);
 
+    let karlkim = Person {
+        first_name: String::from("Karlkim"),
+        last_name: String::from("SK"),
+        age: 45 
+    };
+
+    borrow_person(&karlkim);
+    // borrow_and_mutate_person(&mut karlkim);
+    // cannot borrow `karlkim` as mutable, as it is not declared as mutable
+    move_person(karlkim);
+    // borrow_person(&karlkim);
+    // borrow of moved value: `karlkim`
+
+    let mut mutable_numbers: [i32;3] = [3, 1, 2];
+    println!("original mutable_numbers {:?}", mutable_numbers);
+    mutable_numbers.sort(); // sort(&mut self) -- mutable borrow
+    println!("sorted mutable_numbers {:?}", mutable_numbers);
+    sort_desc(&mut mutable_numbers);
+    println!("sorted desc mutable_numbers {:?}", mutable_numbers);
+}
+
+fn sort_desc(numbers: &mut [i32;3]) {
+    numbers.sort_by(|a, b| b.cmp(a));
 }
 
 fn move_person(_p: Person) {}
