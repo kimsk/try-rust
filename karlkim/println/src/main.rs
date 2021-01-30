497256775a7f8ea0a332af575bb799e742d922e5
karlkim <contact@karlk.im>
2021-01-30 22:13:46 +0700

    pretty print_stock

--- karlkim/println/src/main.rs
+++ karlkim/println/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::fmt;
 use rusty_money::{Money, iso};
 use colored::*;
@@ -31,47 +32,47 @@ impl fmt::Display for Person {
 }
 
 fn main() {
-    let p = Person { name: "Rustacean 🦀", age: 42, team: Team::Red };
-    // positioning
-    println!("team: {2}, name: {0}, age: {1}", p.name, p.age, p.team);
-    // named
-    println!("team: {team}, name: {name}; {motto}",
-        name = p.name,
-        team = p.team,
-        motto = "Come on you Reds!");
+    // let p = Person { name: "Rustacean 🦀", age: 42, team: Team::Red };
+    // // positioning
+    // println!("team: {2}, name: {0}, age: {1}", p.name, p.age, p.team);
+    // // named
+    // println!("team: {team}, name: {name}; {motto}",
+    //     name = p.name,
+    //     team = p.team,
+    //     motto = "Come on you Reds!");
 
-    let p2 = Person { name: "SeeSharp", age: 60, team: Team::Blue };
-    println!("{rust:#?} พบกับ {csharp:#?}", rust = p, csharp = p2);
+    // let p2 = Person { name: "SeeSharp", age: 60, team: Team::Blue };
+    // println!("{rust:#?} พบกับ {csharp:#?}", rust = p, csharp = p2);
 
-    // fmt::Display
-    println!("{}", p);
-    // fmt::Debug
-    println!("{:?}", p);
-    // pretty printing
-    println!("{:#?}", p);
-    // pointer address of the reference
-    println!("{:p}", &p);
-    // binary, hexadecimal, octal
-    println!("Binary: {:b}, hexadecimal: {:x}, octal: {:o}", p.age, p.age, p.age);
-    // unicode as hexadecimal
-    println!("{:X}", 'ก' as u32); // E01
+    // // fmt::Display
+    // println!("{}", p);
+    // // fmt::Debug
+    // println!("{:?}", p);
+    // // pretty printing
+    // println!("{:#?}", p);
+    // // pointer address of the reference
+    // println!("{:p}", &p);
+    // // binary, hexadecimal, octal
+    // println!("Binary: {:b}, hexadecimal: {:x}, octal: {:o}", p.age, p.age, p.age);
+    // // unicode as hexadecimal
+    // println!("{:X}", 'ก' as u32); // E01
 
-    // mix and match
-    // pretty print with hexadecimal
-    println!("{:#x?}", p);
-    // fmt::Debug with named
-    println!("{person:?}", person = p);
+    // // mix and match
+    // // pretty print with hexadecimal
+    // println!("{:#x?}", p);
+    // // fmt::Debug with named
+    // println!("{person:?}", person = p);
 
-    // padding
-    let stocks = vec![
-        ("AMZN", 3_290_15, 3_294_95),
-        ("GOOGL", 1885_00, 1902_09),
-        ("FB", 275_70, 276_20) 
-    ];
+    // // padding
+    // let stocks = vec![
+    //     ("AMZN", 3_290_15, 3_294_95),
+    //     ("GOOGL", 1885_00, 1902_09),
+    //     ("FB", 275_70, 276_20) 
+    // ];
 
-    for (ticker, bid, ask) in stocks {
-        print_stock(ticker, bid, ask);
-    }
+    // for (ticker, bid, ask) in stocks {
+    //     print_stock(ticker, bid, ask);
+    // }
 // =======================AMZN=======================
 // |          bid           |          ask          |
 // -------------------------+------------------------
@@ -87,10 +88,27 @@ fn main() {
 // -------------------------+------------------------
 // |        $275.70         |        $276.20        |
 // -------------------------+------------------------
+
+    //print_padding();
+    let stocks = vec![
+        ("AMZN", 3_290_15, 3_294_95),
+        ("GOOGL", 1885_00, 1902_09),
+        ("FB", 275_70, 276_20),
+        ("GME", 312_00, 315_00)
+    ];
+
+    for (ticker, bid, ask) in stocks {
+        print_stock(ticker, bid, ask);
+    }
+
+    //
+    // println!("Hello {1} is {2:.0$}", 5, "x", 5100.26);
+    // println!("Hello {} is {:.*}", "x", 5, 0.01);
+
 }
 
 // padding sample
-fn print_stock(ticker: &str, bid: i64, ask: i64) {
+fn old_print_stock(ticker: &str, bid: i64, ask: i64) {
     // total length 50, ticker in the middle, pad left and right with =
     println!("{:=^50}", ticker.blue().bold());
     // |
@@ -106,4 +124,53 @@ fn print_stock(ticker: &str, bid: i64, ask: i64) {
     let ask = Money::from_minor(ask, iso::USD);
     println!("|{: ^24}|{: ^23}|", bid.to_string(), ask.to_string());
     println!("{:-^25}+{:-^24}", "", "");
+}
+
+//0111111111111111111100AMZN00111111111111111111110
+
+fn print_stock(ticker: &str, bid: i64, ask: i64) {
+    let stock = format!(" {} ", ticker).bold();
+    let table_width = 50;
+    let ticker_width = stock.len();
+    let left_width = (table_width - ticker_width)/2;
+    let right_width = (table_width - ticker_width)/2;
+    let bid = Money::from_minor(bid, iso::USD);
+    let ask = Money::from_minor(ask, iso::USD);
+
+    println!("table_length: {}", table_width);
+    println!("ticker_width: {}", ticker_width);
+    println!("left_width: {}", left_width);
+    println!("right_width: {}", right_width);
+
+    println!("{:>left_width$}{:─^ticker_width$}{:<right_width$}", "┌", "", "┐", left_width=left_width, right_width=right_width, ticker_width=ticker_width);
+    println!("┌{:─>left_width$}┤{}├{:─>right_width$}┐", "", stock, "", left_width=left_width-2, right_width=right_width-2);
+    println!("│{: ^left_width$}└{:─^ticker_width$}┘{: ^right_width$}│", "bid", "┬", "ask", left_width=left_width-2, right_width=right_width-2, ticker_width=ticker_width);
+    println!("├{:─^left_width$}{:─^ticker_width$}{:─^right_width$}┤", "", "┼", "", left_width=left_width-1, right_width=right_width-1, ticker_width=ticker_width);
+    println!("│{: ^left_width$}{: ^ticker_width$}{: ^right_width$}│", bid.to_string(), "│", ask.to_string(), left_width=left_width-1, right_width=right_width-1, ticker_width=ticker_width);
+    println!("└{:─^left_width$}{:─^ticker_width$}{:─^right_width$}┘", "", "┴", "", left_width=left_width-1, right_width=right_width-1, ticker_width=ticker_width);
+}
+
+
+// padding
+fn print_padding() {
+    // stock variable
+    // :
+    // padding character: =
+    // padding alignment: ^ (middle)
+    // padding size: 50
+    println!("{stock:=^50}", stock = "GME".blue().bold());
+
+    println!("{stock:=<50}", stock = "GME".blue().bold());
+
+    println!("{stock:=>50}", stock = "GME".blue().bold());
+
+    // "|"
+    // " " padding right < until 25
+    println!("{: <25}|{: >24}", "|", "|");
+    println!("|{: ^24}|{: ^23}|", "bid", "ask");
+
+    println!("{:-^25}+{:-^24}", "", "");
+
+    // new line
+    println!("{:\n^5}", "")
 }
