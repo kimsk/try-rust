02c65ef5f18149fd529ce9cdf5a7cfef74d806fb
karlkim <contact@karlk.im>
2021-02-05 16:29:45 +0700

    generics

---
+++ The-Rust-Programming-Language/generics/src/main.rs
@@ -0,0 +1,105 @@
+#![allow(dead_code, unused_variables)]
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// fn largest<T>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+// T, U, V, W
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+
+
+    // let result = largest(&number_list);
+    // println!("The largest number is {}", result);
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+
+    println!("distance from origin = {}", both_float.distance_from_origin());
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2); 
+    // V is &str, W is char
+    // T is i32, U is f64
+    // p3.x is T is i32, p3.y is W is char
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+}
+
+// monomorphization -> no run-time cost
+// Option<T> -> Option_i32 & Option_f64 if compiler found Some(5) and Some(5.0)
