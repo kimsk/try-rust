726494575c9d7e3333e642a2c7327eaae903f507
karlkim <contact@karlk.im>
2021-02-20 17:31:11 +0700

    minigrep -- case sensitive search

--- The-Rust-Programming-Language/minigrep/src/lib.rs
+++ The-Rust-Programming-Language/minigrep/src/lib.rs
@@ -1,11 +1,18 @@
 use std::error::Error;
+use std::env;
 use std::fs;
 
 // Box<dyn Error> means the function will return a type that implements the Error trait, but we don’t have to specify what particular type the return value will be.
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -28,12 +35,28 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results 
 }
 
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
 // https://stackoverflow.com/questions/57234140/how-to-assert-errors-in-rust
 // Result<T, E> only implements PartialEq when T and E also implement PartialEq
 #[derive(Debug, PartialEq)]
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
@@ -44,7 +67,10 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        //  CASE_INSENSITIVE=1 cargo run to poem.txt
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
@@ -64,9 +90,10 @@ mod tests {
     fn enough_arguments() {
         let query = "query";
         let filename = "filename";
+        let case_sensitive = true;
         let args = ["minigrep".to_string(), String::from(query), String::from(filename)];
         let actual = Config::new(&args);
-        let expected: Result<Config, &'static str> = Ok(Config { query: query.to_string(), filename: filename.to_string()});
+        let expected: Result<Config, &'static str> = Ok(Config { query: query.to_string(), filename: filename.to_string(), case_sensitive});
         assert_eq!(expected, actual);
     }
 
@@ -81,4 +108,30 @@ Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
